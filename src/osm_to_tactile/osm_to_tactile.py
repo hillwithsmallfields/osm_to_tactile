601018eadfdfe46c0dce70a9b00db619a2934504
John C. G. Sturdy <jcg.sturdy@gmail.com>
2025-12-08 10:14:34 +0000

    checkpoint

--- src/osm_to_tactile/osm_to_tactile.py
+++ src/osm_to_tactile/osm_to_tactile.py
@@ -35,7 +35,14 @@ OSM_DEBUG_FORMAT = "https://www.openstreetmap.org/?mlat=%f&mlon=%f#map=16/%f/%f"
 
 class LinearWay:
 
+    """Common parent class for all linear ways (streets, pavements, crossings)."""
+
+    def __init__(self, geometry=None, width=1):
+        self.geometry = geometry
+        self.width = width
+
     def coords(self):
+        """Return the coordinate list for this way."""
         try:
             return list(self.geometry.coords)
         except NotImplementedError as e:
@@ -44,9 +51,18 @@ class LinearWay:
             print("geometry.geoms is", self.geometry.geoms)
 
     def solid(self):
+        """Return a 2D solid representing this way.
+
+        A way has no width; the solid form of it has width."""
         return shapely.buffer(self.geometry, self.width / 2)
 
 class Street(LinearWay):
+
+    """A street or road.
+
+    This is a linear feature, and does not include streets which are
+    areas.
+    """
 
     def __init__(self,
                  geometry,
@@ -55,14 +71,15 @@ class Street(LinearWay):
                  name=None,
                  subtype=None,
                  ):
+        super().__init__(geometry=geometry,
+                         width=4, # TODO: set according to street data
+                         )
         self.attributes = attributes
         self.name = name or attributes.get('name', "<anon>")
         self.subtype = subtype or attributes.get('highway')
         if not attributes:
             self.attributes = {'name': self.name,
                                'subtype': self.subtype}
-        self.geometry = geometry
-        self.width = 4          # TODO: set according to street data
 
     def __str__(self):
         return f"<Street {self.subtype} {self.name} {self.geometry}>"
@@ -75,12 +92,15 @@ class Street(LinearWay):
 
 class Pavement(LinearWay):
 
+    """A pavement alongside a street (sidewalk in US English)."""
+
     def __init__(self,
                  geometry,
                  type=None,
                  ):
-        self.geometry = geometry
-        self.width = 1
+        super().__init__(geometry=geometry,
+                         width=1,
+                         )
 
     def __str__(self):
         return f"<Pavement {self.geometry}>"
@@ -91,12 +111,15 @@ class Pavement(LinearWay):
 
 class Crossing(LinearWay):
 
+    """A pedestrian crossing such as a zebra crossing."""
+
     def __init__(self,
                  geometry,
                  type=None,
                  ):
-        self.geometry = geometry
-        self.width = 2
+        super().__init__(geometry=geometry,
+                         width=2,
+                         )
 
     def __str__(self):
         return f"<Crossing {self.geometry}>"
@@ -108,8 +131,8 @@ class Crossing(LinearWay):
 PRETTY_PRINT = True
 
 def write_svg(output, bbox, streets, pavements, crossings):
+    """Write the map as SVG."""
     # TODO: flip rotate coordinates
-    print("writing output to", output)
     left, bottom, right, top = bbox
     width = right - left
     height = top - bottom
@@ -125,24 +148,30 @@ def write_svg(output, bbox, streets, pavements, crossings):
         outstream.write("</svg>\n")
 
 def write_dxf(output, bbox, streets, pavements, crossings):
+    """Write the map as DXF."""
     pass
 
-def coords(transformer, base_x, base_y, limit_x, limit_y, geometry):
+def coords(transformer, clip_rect, geometry):
+    """Transform all the coordinates in a geometry, using a given transformer.
+    This works whether the geometry has a single line or multiple lines.
+    The result is clipped to be within the rectangle specified."""
     def transform_xy_list(xy_list):
-        return [(x-base_x, y-base_y)
+        """Translate and scale a list of XY coordinate pairs."""
+        return [(x-clip_rect[0], y-clip_rect[1])
                 for x, y in (transformer.transform(lon, lat)
                              for lon, lat in xy_list)]
     match geometry['type']:
         case 'LineString':
-            unclipped = shapely.LineString(transform_xy_list(geometry['coordinates']))
-            result = shapely.clip_by_rect(unclipped,
-                                          0, 0,
-                                          limit_x-base_x, limit_y-base_y,
-                                          )
-            return result
+            return shapely.clip_by_rect(shapely.LineString(transform_xy_list(geometry['coordinates'])),
+                                        0, 0,
+                                        clip_rect[2]-clip_rect[0], clip_rect[3]-clip_rect[1],
+                                        )
         case 'Polygon':
-            return shapely.Polygon([transform_xy_list(shape)
-                                    for shape in geometry['coordinates']])
+            return shapely.clip_by_rect(shapely.Polygon([transform_xy_list(shape)
+                                                         for shape in geometry['coordinates']]),
+                                        0, 0,
+                                        clip_rect[2]-clip_rect[0], clip_rect[3]-clip_rect[1],
+                                        )
         case _:
             print("Unknown geometry type", _)
 
@@ -164,21 +193,33 @@ def show(streets, pavements, crossings):
     for crossing in crossings:
         print("    ", crossing)
 
-def osm_fetch_streets_in_bbox(west, south, east, north, verbose=False):
+def osm_fetch_streets_in_bbox(input_bbox,
+                              projection="EPSG:3857",
+                              verbose=False):
+    """Fetch all the streets, pavements and crossings in the given rectangle.
+
+    The output is in the specified projection, which by default is Web Mercator.
+
+    The results, all in the projection coordinates, are:
+    - the rectangle bounds
+    - the streets
+    - the pavements
+    - the crossings
+    """
     if verbose:
-        print("fetching data in", west, south, east, north)
-        print("southwest", OSM_DEBUG_FORMAT % (south, west, south, west))
-        print("northeast", OSM_DEBUG_FORMAT % (north, east, north, east))
+        print("fetching data in", input_bbox[0], input_bbox[1], input_bbox[2], input_bbox[3])
+        print("southwest", OSM_DEBUG_FORMAT % (input_bbox[1], input_bbox[0], input_bbox[1], input_bbox[0]))
+        print("northeast", OSM_DEBUG_FORMAT % (input_bbox[3], input_bbox[2], input_bbox[3], input_bbox[2]))
     overpass = Overpass()
-    query = overpassQueryBuilder(bbox=[south, west, north, east],
+    query = overpassQueryBuilder(bbox=input_bbox,
                                  elementType='way',
                                  selector='highway',
                                  includeGeometry=True,
                                  out='body')
     osm_data = overpass.query(query)
 
-    transformer = pyproj.Transformer.from_crs("EPSG:4326", "EPSG:3857")
-    left, bottom, right, top = transformer.transform_bounds(west, south, east, north)
+    transformer = pyproj.Transformer.from_crs("EPSG:4326", projection)
+    output_bbox = transformer.transform_bounds(input_bbox[0], input_bbox[1], input_bbox[2], input_bbox[3])
 
     ways = osm_data.ways()
     streets = defaultdict(list)
@@ -193,20 +234,23 @@ def osm_fetch_streets_in_bbox(west, south, east, north, verbose=False):
         if tags.get('highway') == 'footway':
             match tags.get('footway'):
                 case 'sidewalk':
-                    pavements.append(Pavement(coords(transformer, left, bottom, right, top, geometry)))
+                    pavements.append(Pavement(coords(transformer, output_bbox, geometry)))
                 case 'crossing':
-                    crossings.append(Crossing(coords(transformer, left, bottom, right, top, geometry)))
+                    crossings.append(Crossing(coords(transformer, output_bbox, geometry)))
         else:
-            street = Street(attributes=tags, geometry=coords(transformer, left, bottom, right, top, geometry))
+            street = Street(attributes=tags, geometry=coords(transformer, output_bbox, geometry))
             streets[street.name].append(street)
-    return [left, bottom, right, top], streets, pavements, crossings
+    return output_bbox, streets, pavements, crossings
 
-def convert_to_islands(streets, pavements, crossings):
+def convert_to_islands(streets, pavements=None, crossings=None):
+    """Convert the solid ways to a probably contiguous area, with islands in it,
+    where each island is the area between streets (or between a street and its pavements).
+    If used for cutting, this will result in a cut sheet which can be stuck to a baseboard."""
     return shapely.union_all([s.solid()
                               for sg in streets.values()
                               for s in sg]
-                             + [p.solid() for p in pavements]
-                             + [c.solid() for c in crossings])
+                             + [p.solid() for p in pavements] if pavements else []
+                             + [c.solid() for c in crossings] if pavements else [])
 
 def osm_to_tactile_main(
         bbox=None,
@@ -233,7 +277,7 @@ def osm_to_tactile_main(
         south = latitude - size_scale * (height/2)
         east = longitude + size_scale * (width/2)
         north = latitude + size_scale * (height/2)
-    bbox, streets, pavements, crossings = osm_fetch_streets_in_bbox(west, south, east, north)
+    bbox, streets, pavements, crossings = osm_fetch_streets_in_bbox([west, south, east, north])
     if verbose:
         show(streets, pavements, crossings)
     print("output is", output)
